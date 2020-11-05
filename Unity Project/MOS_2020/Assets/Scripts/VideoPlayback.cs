e0a538194513ae8876f8e6f756edbf72ab666b4a
VoxAndrews <VoxAndrews@users.noreply.github.com>
2020-11-05 15:15:39 +0800

    Started working on Video Playback

---
+++ Unity Project/MOS_2020/Assets/Scripts/VideoPlayback.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayback : MonoBehaviour
+{
+    [SerializeField]
+    GameObject renderObject;
+    [SerializeField]
+    Material videoMaterial;
+    [SerializeField]
+    AudioSource audioPlayback;
+    [SerializeField]
+    string webAddress;
+    [SerializeField]
+    VideoPlayer videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(videoPlayer == null)
+        {
+            videoPlayer = gameObject.GetComponent<VideoPlayer>();
+        }
+        
+        if(videoPlayer.renderMode != UnityEngine.Video.VideoRenderMode.RenderTexture)
+        {
+            videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.RenderTexture;
+        }
+
+        videoMaterial.SetTexture("_MainTex", videoPlayer.targetTexture);
+        videoMaterial.SetTexture ("_EmissionMap", videoPlayer.targetTexture);
+
+        renderObject.GetComponent<MeshRenderer>().material = videoMaterial;
+
+        if(videoPlayer.audioOutputMode != UnityEngine.Video.VideoAudioOutputMode.AudioSource)
+        {
+            videoPlayer.audioOutputMode = UnityEngine.Video.VideoAudioOutputMode.AudioSource;
+        }
+
+        videoPlayer.SetTargetAudioSource(0, audioPlayback);
+
+        videoPlayer.url = webAddress;
+
+        videoPlayer.Prepare();
+
+        videoPlayer.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
