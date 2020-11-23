f7b2204c55e824aef16bf123823630ad2e854a61
VoxAndrews <VoxAndrews@users.noreply.github.com>
2020-11-23 18:20:06 +0800

    Updated the MOS_2020_start Scene
    Finishing up the Skeleton of the scene, then I will begin the process of placing objects

---
+++ Unity Project/MOS_2020/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    public GameObject gameManage;
+    public GameObject fadeInBlackWhite;
+    public bool startup = false;
+
+    void Awake() 
+    {
+        if (gameManage == null)
+        {
+            gameManage = GameObject.Find("GameManager");
+        }
+
+        if (fadeInBlackWhite == null)
+        {
+            fadeInBlackWhite = GameObject.Find("fadeIn_blackWhiteClear");
+        }
+
+        if(startup == true)
+        {
+            StartCoroutine(FadeInBlackToWhite());
+        }
+    }
+
+    IEnumerator FadeInBlackToWhite() 
+    {
+        Debug.Log("Disabling User Input");
+        gameManage.GetComponent<GameManager>().DisableInputs();
+
+        Debug.Log("Started Coroutine at timestamp: " + Time.time);
+        Debug.Log("Fading in from Black to White");
+
+        fadeInBlackWhite.GetComponent<Animator>().Play("fadeIn_blackWhiteClear");
+
+        // Yields for the length of the animation clip
+        yield return new WaitForSeconds(2.5f);
+
+        Debug.Log("Finished Coroutine at timestamp: " + Time.time);
+
+        Debug.Log("Enabling User Input");
+        gameManage.GetComponent<GameManager>().EnableInputs();
+    }
+}
---
+++ Unity Project/MOS_2020/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject fpsObject;
+
+    void Awake()
+    {
+        if(fpsObject == null)
+        {
+            fpsObject = GameObject.Find("FPSController");
+        }
+
+        HideAndLockCursor();
+    }
+
+    public void HideAndLockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void DisableInputs()
+    {
+        fpsObject.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().disableInput = true;
+    }
+
+    public void EnableInputs()
+    {
+        fpsObject.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().disableInput = false;
+    }
+}
